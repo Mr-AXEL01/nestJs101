ce3ab82ddc46574fec1f4a9ba2107d650521e860
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-05-06 14:58:20 +0100

    set findAll function so it will check if there is a role in the search or filter to can get anly the role that we filtreing with or if there is no role it will get all the users

--- lesson01/src/users/users.service.ts
+++ lesson01/src/users/users.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users = [
+        { 
+            id: 1, 
+            name: "John Doe", 
+            email: "john@example.com", 
+            role: "ADMIN" 
+        },
+
+        { 
+            id: 2, 
+            name: "Jane Smith", 
+            email: "jane@example.com", 
+            role: "MANAGER" 
+        },
+
+        { 
+            id: 3, 
+            name: "Alice Johnson", 
+            email: "alice@example.com", 
+            role: "DEVELOPER" 
+        },
+
+        { 
+            id: 4, 
+            name: "Bob Brown", 
+            email: "bob@example.com", 
+            role: "DEVELOPER" 
+        },
+
+        { 
+            id: 5, 
+            name: "Emily Davis", 
+            email: "emily@example.com", 
+            role: "DEVELOPER" 
+        }
+    ];
+
+
+    findAll(role?: 'DEVELOPER' | 'MANAGER' | 'ADMIN') 
+    {
+        if(role) {
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    
+
+
+}
