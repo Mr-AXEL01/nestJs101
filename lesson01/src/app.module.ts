c4e6110c3e91826b23bdce26fdfaeca258d14478
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-05-08 03:06:36 +0100

    set the module of Products

--- lesson01/src/app.module.ts
+++ lesson01/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb+srv://MrAxel-01:3GqpiUBaO5NAkh6g@mraxel-01.fb1qxgu.mongodb.net/learning?retryWrites=true&w=majority') , UsersModule, AuthModule],
-  controllers: [AppController],
+  imports: [ MongooseModule.forRoot('mongodb+srv://MrAxel-01:3GqpiUBaO5NAkh6g@mraxel-01.fb1qxgu.mongodb.net/learning?retryWrites=true&w=majority') , UsersModule, AuthModule, ProductsModule],
+  controllers: [AppController, ProductsController],
   providers: [AppService],
 })
 export class AppModule {}
