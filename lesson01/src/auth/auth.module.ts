5cbdb0c76af0b7e0ab82153583445af1f39727ce
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-05-08 10:53:57 +0100

    set the expiration time to be one hour

--- lesson01/src/auth/auth.module.ts
+++ lesson01/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { jwtConstants } from './contstants';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '3600s' },
     }),
   ],
   providers: [AuthService],
